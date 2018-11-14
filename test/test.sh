22f8274ee81af35dd67e08840883a4745b88bc06
Nimlar <nimlar@gmail.com>
2018-11-14 19:59:21 +0100

    test.sh: generate all files to compare with previous move def

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,21 @@
-make gcc MOVE=aguet && ./emul-gcc |grep "" |grep -E "(write)|(Attach)" >/tmp/log.txt && grep -vR "###" /tmp/log.txt > /tmp/new.txt && grep -E "^###" /tmp/log.txt | sed -e "s,^###,,"  > /tmp/old.txt
+#!/bin/bash
+
+MOVES=("triste" "penaud" "gauche" "droit" "aguet" "content" "ecoute" "surprise" "baisse" "tourne" "reset")
+MOVES=("triste")
+FILTERS=("RightAzi" "RightAlt" "Left Azi" "Left Alt")
+
+DIR=/tmp/catears
+mkdir -p "$DIR"
+
+for MOVE in "${MOVES[@]}"; do
+	make gcc MOVE="$MOVE"
+	mkdir -p "$DIR/$MOVE"
+	./emul-gcc > "$DIR/$MOVE/log.txt"
+	grep -vR "###" "$DIR/$MOVE/log.txt" > "$DIR/${MOVE}/new.txt" 
+	grep -E "^###" "$DIR/$MOVE/log.txt" | sed -e "s,^###,,"  > "$DIR/${MOVE}/old.txt"
+
+	for FILTER in "${FILTERS[@]}"; do
+	 	grep "$FILTER" "$DIR/${MOVE}/new.txt" |grep -E "(write)|(Attach)" > "$DIR/$MOVE/${FILTER}_new.txt" 
+	 	grep "$FILTER" "$DIR/${MOVE}/old.txt" |grep -E "(write)|(Attach)" > "$DIR/$MOVE/${FILTER}_old.txt"
+	done
+done
